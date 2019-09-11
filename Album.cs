3cf7bf6d71331cf97550bcb4490a353a0f63dcd7
Cornelia1991 <Cornelia1991@users.noreply.github.com>
2019-09-10 20:02:47 -0400

    Practice method overloading, if statements and string concatentation

--- Album.cs
+++ Album.cs
@@ -4,6 +4,8 @@
     {
         public readonly string Title;
         public readonly string Artist;
+        public string Loanee;
+        public bool OnLoan;
 
         public Album(string title, string artist)
         {
@@ -13,7 +15,37 @@
 
         public string GetDisplayText()
         {
-            return "Album: " + Title + " by " + Artist;
+           string text = "Album: " + Title + " by " + Artist;
+            if (OnLoan)
+            {
+                if (!string.IsNullOrEmpty(Loanee))
+                {
+                    text += "(Currently on loan to " + Loanee + ")";
+                }
+                else
+                {
+                    text += "(Currently on loan)";
+                }
+            }
+            return text;
+        }
+
+        public void Loan(string loanee)
+        {
+            Loanee = loanee;
+            Loan();
+        }
+
+        public void Loan()
+        {
+            OnLoan = true;
+        }
+
+        public void Return()
+        {
+            //default values
+            Loanee = null;
+            OnLoan = false;
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -13,6 +13,16 @@ namespace Treehouse.MediaLibrary
             Console.WriteLine(album1.GetDisplayText());
             Console.WriteLine(album2.GetDisplayText());
             Console.WriteLine(album3.GetDisplayText());
+
+            album1.Loan("Mary");
+            Console.WriteLine(album1.GetDisplayText());
+
+            album1.Return();
+            Console.WriteLine(album1.GetDisplayText());
+
+            album1.Loan();
+            Console.WriteLine(album1.GetDisplayText());
+
             Console.ReadLine();
         }
     }
